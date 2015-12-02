62a2513b3e79a825f6e076c2cf0f0d231e57bc98
VikramBombhi <vbombhi@gmail.com>
2015-12-02 00:56:40 -0500

    added projects

--- javascript.js
+++ javascript.js
@@ -10,3 +10,33 @@ $(window).scroll(function() {
         $('nav').removeClass("unpadded");
     }
 });
+
+
+$(document).ready(function() {
+
+  function close_accordion_section() {
+    // Close everything up
+    $('.accordion .accordion-section-title').removeClass('active');
+    $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+  }
+
+  $('.accordion-section-title').click(function(e) {
+    // Grab current anchor value
+    var currentAttrValue = $(this).attr('href');
+    //Change the icon
+    $(this).find('span').toggleClass('fa-angle-right fa-angle-down')
+    // Open and close here
+
+    if($(this).is('.active')) {
+      close_accordion_section();
+    }
+    else {
+      close_accordion_section();
+      $(this).addClass('active');
+      $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+    }
+
+    e.preventDefault();
+
+  });
+});
